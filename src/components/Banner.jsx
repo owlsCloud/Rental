6e5cc15ad343ad6c4638b82a8358d467b395fc1a
Gerardo Garza <owlscloud@gmail.com>
2023-03-19 00:54:24 -0500

    a

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ function Banner() {
       className="text-center m-auto text-white mt-20 py-16 "
       style={{ backgroundColor: "#2D2D2D" }}
     >
-      <h2 className="text-5xl">Save big with our cheap car rental!</h2>
+      <h2 className="text-5xl mb-4">Save big with our cheap car rental!</h2>
       <p className="text-2xl">
         Top Airports. Local Suppliers
         <span className="text-primary"> 24/7</span> Support.
